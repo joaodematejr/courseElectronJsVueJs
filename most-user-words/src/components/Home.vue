236a8f8ef8e1b2e3a6062867b68c23433f4244c2
João Dematé Jr <joaodematejr@gmail.com>
2021-07-19 20:11:07 -0300

    Criação de componentes

---
+++ most-user-words/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+      <Pill
+        v-for="word in groupeWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill";
+export default {
+  components: { Pill },
+  data: function () {
+    return {
+      groupeWords: [
+        { name: "you", amount: 900 },
+        { name: "satan", amount: 666 },
+        { name: "i", amount: 665 },
+        { name: "he", amount: 668 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ most-user-words/src/components/Pill.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span>
+      {{ name }}
+    </span>
+    <span>
+      {{ amount }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"],
+};
+</script>
+
+<style>
+</style>
