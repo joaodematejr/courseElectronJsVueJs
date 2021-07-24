a7f151617fda43618e677c78d381264e8a24a6bb
João Dematé Jr <joaodematejr@gmail.com>
2021-07-23 22:25:53 -0300

    Implementação função pathsToRows

--- most-user-words/src/backend/index.js
+++ most-user-words/src/backend/index.js
@@ -1,11 +1,16 @@
 const { ipcMain } = require("electron");
 
+const pathsToRows = require("./pathsToRows");
+
 ipcMain.on("process-subtitles", (event, paths) => {
-  console.log(paths);
-  event.reply("process-subtitles", [
-    { name: "you", amount: 900 },
-    { name: "satan", amount: 666 },
-    { name: "i", amount: 665 },
-    { name: "he", amount: 668 },
-  ]);
+  pathsToRows(paths)
+    .then((rows) => console.log(rows))
+    .then(() => {
+      event.reply("process-subtitles", [
+        { name: "you", amount: 900 },
+        { name: "satan", amount: 666 },
+        { name: "i", amount: 665 },
+        { name: "he", amount: 668 },
+      ]);
+    });
 });
